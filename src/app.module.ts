5966a8bf123bba07a8718c5234f81781f7946cbb
p-ej <show400035@naver.com>
2021-10-22 18:25:12 +0900

    controller test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { UsersController } from './users/users.controller';
+import { AdminController } from './admin/admin.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, UsersController],
+  controllers: [AppController, CatsController, UsersController, AdminController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { stringify } from 'querystring';
 
 @Controller('cats')
 export class CatsController {
 
-    @Get('test')
-    findAll() : string {
-        return 'This actin returns all cats';
+    // Post 엔드포인트 테스트
+    // @Post()
+    // @HttpCode(204)
+    // create(){
+    //     return 'This action adds a new cat Post';
+    // }
+    
+    // @Post()
+    // create() : string{
+    //     return 'This action adds a new cat Post';
+    // }
+
+    @Post()
+    @Header('Cache-Control', 'none')
+    create(){
+        return 'This action adds a new cat Post headers';
     }
+    // Get 엔드포인트 테스트 or 패턴 기반 라우트
+    @Get('ab*cd')
+    findAll(@Req() request : Request) : string {
+        console.log(request);
+        return 'This actin returns all cats or pattern route';
+    }
+
+    @Get(':id')
+    findOne(@Param() params) : string{
+        console.log(params.id);
+        return 'This action returns a #${params.id}'+ params.id +'cat';
+    }
+
+    @Get()
+    @Redirect('https://nestjs.com', 301)
+    redirectMethod(){}
+
+    // @Get('docs')
+    // @Redirect('https://docs.nestjs.com', 302)
+    // getDocs(@Query('version') version){
+    //     if(version && version === '5'){
+    //         return {url : 'https://docs.nestjs.com/v5/'}
+    //     }
+    // }
 }
